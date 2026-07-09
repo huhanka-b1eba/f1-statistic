04667118f9b2d88eb41964dff9bfba8abb84e3e5
Aigiz Tulubaev <flaylay256@gmail.com>
2026-07-09 14:59:18 +0300

    [F1-11] add folders in session page

--- src/entities/session/index.ts
+++ src/entities/session/index.ts
@@ -2,4 +2,9 @@ export { default as SessionCard } from "./ui/session-card"
 export type { SessionListItem } from "./model/types"
 export { getSessionCardViewTransitionName } from "./model/view-transition"
 
-export { useGetMeetingsQuery, useGetSessionsQuery, useGetDashboardBySessionKeyQuery } from "./api"
+export {
+    useGetMeetingsQuery,
+    useGetSessionsQuery,
+    useGetDashboardBySessionKeyQuery,
+    useGetPositionsBySessionKeyQuery,
+} from "./api"
